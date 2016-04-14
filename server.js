9333bc9c045aee76c54b37c4011b7ef53f17bcb3
Mike Brevoort <mike.brevoort@robotsandpencils.com>
2016-04-13 20:25:38 -0600

    trying botkit, simplify

--- server.js
+++ server.js
@@ -1,9 +1,3 @@
-var express = require('express')
-var app = express()
-var bodyParser = require('body-parser')
-var request = require('request')
-
-app.use(bodyParser.json())
 var token = process.env.TOKEN
 var verifyToken = process.env.VERIFY_TOKEN
 var port = process.env.PORT
@@ -12,48 +6,24 @@ if (!token) throw new Error('TOKEN is required but missing')
 if (!verifyToken) throw new Error('VERIFY_TOKEN is required but missing')
 if (!port) throw new Error('PORT is required but missing')
 
-function sendTextMessage (sender, text) {
-  var messageData = { text: text }
-  request({
-    url: 'https://graph.facebook.com/v2.6/me/messages',
-    qs: {access_token: token},
-    method: 'POST',
-    json: {
-      recipient: { id: sender },
-      message: messageData
-    }
-  }, function (error, response, body) {
-    if (error) {
-      console.log('Error sending message: ', error)
-    } else if (response.body.error) {
-      console.log('Error: ', response.body.error)
+var Botkit = require('botkit')
+var controller = Botkit.facebookbot({ debug: true, access_token: token, verify_token: verifyToken })
+var bot = controller.spawn()
+
+controller.setupWebserver(port, function (err, webserver) {
+  if (err) return console.log(err)
+  controller.createWebhookEndpoints(webserver, bot, function () {
+    console.log('Ready Player 1')
+  })
+})
+
+controller.hears(['hello', 'hi'], 'message_received', function (bot, message) {
+  controller.storage.users.get(message.user, function (err, user) {
+    if (err) return console.log(err)
+    if (user && user.name) {
+      bot.reply(message, 'Hello ' + user.name + '!!')
+    } else {
+      bot.reply(message, 'Hello.')
     }
   })
-}
-
-app.get('/fb', function (req, res) {
-  if (req.query['hub.verify_token'] === verifyToken) {
-    res.send(req.query['hub.challenge'])
-    return
-  }
-  res.send('Error, wrong validation token')
-})
-
-app.post('/fb', function (req, res) {
-  console.log('POST')
-  var messaging_events = req.body.entry[0].messaging
-  for (var i = 0; i < messaging_events.length; i++) {
-    var event = req.body.entry[0].messaging[i]
-    var sender = event.sender.id
-    if (event.message && event.message.text) {
-      var text = event.message.text
-      console.log('receieved: ', text)
-      sendTextMessage(sender, text)
-    }
-  }
-  res.sendStatus(200)
-})
-
-app.listen(port, function () {
-  console.log('listening on port ' + port)
 })
